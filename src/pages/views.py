72707fc5815b407f974fad1936359075aa87f191
Julie Nguyen <julie-nguyen@live.com>
2020-03-13 19:14:02 -0400

    created views

--- src/pages/views.py
+++ src/pages/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
-def home_view():
-  return "<h1>Hello World</h1>" #string of HTML code
+def home_view(request, *args, **kwargs):
+  print(args, kwargs)
+  print(request.user)
+  # return HttpResponse("<h1>Hello World</h1>")
+  return render(request, "home.html", {})
+
+def contact_view(request, *args, **kwargs):
+  return render(request, "contact.html", {})
+
+
+def about_view(request, *args, **kwargs):
+  return render(request, "about.html", {})
+
+def social_view(request, *args, **kwargs):
+  return HttpResponse("<h1>Social Page</h1>")
